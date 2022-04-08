29a2afa9e50d239284fb028929065db257e28dda
AZhufengjiao <2231472240@qq.com>
2022-04-08 15:26:23 +0800

    项目完成

--- wz/src/main.js
+++ wz/src/main.js
@@ -12,22 +12,22 @@ import  store from '@/store'
 
 createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
 
-// 路由守卫
-router.beforeEach((to,from,next)=>{
-  let token=store.state.login.token
-  if(token){  // 如果有token值，并且要退回到登录页面，通过next 跳回到/
-   if(to.path==='/login'){
-    next({
-     path:'/'
-    })
-   }else{  // 要到其他得页面就直接通过
-     next()
-   }
-  }else{  // 如果没有token值，并且在login页就直接放行到login页面
-    if(to.path==='/login'){
-     next()
-    }else{
-     next('/login')  // 直接跳回到login
-    }
-  }
-})
+// // 路由守卫
+// router.beforeEach((to,from,next)=>{
+//   let token=store.state.login.token
+//   if(token){  // 如果有token值，并且要退回到登录页面，通过next 跳回到/
+//    if(to.path==='/login'){
+//     next({
+//      path:'/'
+//     })
+//    }else{  // 要到其他得页面就直接通过
+//      next()
+//    }
+//   }else{  // 如果没有token值，并且在login页就直接放行到login页面
+//     if(to.path==='/login'){
+//      next()
+//     }else{
+//      next('/login')  // 直接跳回到login
+//     }
+//   }
+// })
